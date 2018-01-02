3dc1831a242c88b90c8fcc9a54581e846c8f9a8d
ajayk1234 <ajaykangare1993@gmail.com>
2018-01-02 09:39:07 +0530

    resolved show video error

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -24,9 +24,9 @@ class VideosController < ApplicationController
   end
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
- 
+  
   private
- 
+  
   def user_not_authorized
     flash[:warning] = "You are not authorized to perform this action."
     redirect_to(request.referrer || videos_path)
